fe8045e101e602065baf2d2ea64fcbc117c386e5
BhargaviSingh <bhargavi.singh0711@gmail.com>
2024-03-04 03:45:42 +0530

    calculator prg added

--- IdeaProjects/justcheckin/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hey there");
-        //just working with git
-        //changes2.0
-    }
-}
---
+++ IdeaProjects/justcheckin/src/calculatorPrg.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class calculatorPrg {
+    public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int ans=0;
+            while(true){
+                System.out.println("enter an operator");
+                char op=sc.next().trim().charAt(0);
+
+                if(op=='+' || op=='-' || op=='*' || op=='/' || op=='%') {
+                    System.out.println("enter two operands");
+                    int num1 = sc.nextInt();
+                    int num2 = sc.nextInt();
+                    if (op == '+')
+                        ans = num1 + num2;
+                    if (op == '-')
+                        ans = num1 - num2;
+                    if (op == '*')
+                        ans = num1 * num2;
+                    if (op == '/'){
+                        if(num2!=0){
+                            ans = num1 / num2;
+                        }
+                    }
+                    if(op == '%'){
+                        ans=num1%num2;
+                    }
+
+                }
+                else if(op=='x' || op=='X'){
+                    break;}
+                else{
+                    System.out.println("Invalid operation");}
+                System.out.println("Result is "+ ans);
+            }
+
+    }
+}
